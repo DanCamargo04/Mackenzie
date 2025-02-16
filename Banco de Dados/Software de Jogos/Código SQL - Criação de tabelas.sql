5c8dce9e35bbe1c97464d82a81dd93fb236b5723
Daniel Rubio Camargo <danrcamargo.code@gmail.com>
2025-02-16 20:38:08 -0300

    Update Código SQL - Criação de tabelas.sql

--- Banco de Dados/Software de Jogos/Código SQL - Criação de tabelas.sql
+++ Banco de Dados/Software de Jogos/Código SQL - Criação de tabelas.sql
@@ -1,33 +1,35 @@
-CREATE TABLE Usuario
-(
-	id_usuario INT NOT NULL,
-  	nome_usuario VARCHAR(20) NOT NULL,
-  	data_cadastro DATE NOT NULL,
+CREATE TABLE Usuario (
 	
-  	PRIMARY KEY (id_usuario)
+    id_usuario INT NOT NULL,
+    nome_usuario VARCHAR(20) NOT NULL,
+    data_cadastro DATE NOT NULL,
+	
+    PRIMARY KEY (id_usuario)
 );
 
-CREATE TABLE Jogo
-(
-	id_jogo INT NOT NULL,
-  	nome_jogo VARCHAR(20) NOT NULL,
-  	preco_jogo VARCHAR(10) NOT NULL,
-  	genero VARCHAR(20),
-  	desenvolvedora VARCHAR(20),
-  	publicadora VARCHAR(20),
+CREATE TABLE Jogo (
+	
+    id_jogo INT NOT NULL,
+    nome_jogo VARCHAR(20) NOT NULL,
+    preco_jogo VARCHAR(10) NOT NULL,
+    genero VARCHAR(20),
+    desenvolvedora VARCHAR(20),
+    publicadora VARCHAR(20),
   	
-  	PRIMARY KEY (id_jogo)
+    PRIMARY KEY (id_jogo)
 );
 
 CREATE TABLE Avaliacao (
+	
     id_avaliacao INT NOT NULL,
     comentario TEXT,
     classificacao INT,
   
-  	PRIMARY KEY (id_avaliacao)
+    PRIMARY KEY (id_avaliacao)
 );
 
 CREATE TABLE Expansao (
+	
     id_expansao INT NOT NULL,
     id_jogo INT NOT NULL,
     nome_expansao VARCHAR(20) NOT NULL,
@@ -37,6 +39,7 @@ CREATE TABLE Expansao (
 );
 
 CREATE TABLE Compra_Jogo (
+	
     id_jogo INT NOT NULL,
     id_usuario INT NOT NULL,
     data_compra_jogo DATE NOT NULL,
@@ -47,6 +50,7 @@ CREATE TABLE Compra_Jogo (
 );
 
 CREATE TABLE Compra_Expansao (
+	
     id_expansao INT NOT NULL,
     id_usuario INT NOT NULL,
     data_compra_expansao DATE NOT NULL,
@@ -57,6 +61,7 @@ CREATE TABLE Compra_Expansao (
 );
 
 CREATE TABLE Avalia (
+	
     id_jogo INT NOT NULL,
     id_avaliacao INT NOT NULL,
     id_usuario INT NOT NULL,
